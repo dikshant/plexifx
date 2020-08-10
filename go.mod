f4532c8e2289ce4454e2cd9ddfb656488a332852
Dikshant Adhikari <dadhikari@sevone.com>
2020-08-09 23:51:02 -0400

    add: env vars for config

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/hekmon/plexwebhooks v0.1.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/spf13/pflag v1.0.5
 	go.uber.org/zap v1.15.0
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,30 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dikshant/plexifx/lifx"
 	"github.com/dikshant/plexifx/webhook"
 	"github.com/kelseyhightower/envconfig"
+	flag "github.com/spf13/pflag"
 	"go.uber.org/zap"
 )
 
 // Config contains address of the webhook listener
 type Config struct {
-	Address                 string        `default:"localhost:6060"`
-	DeviceDiscoveryInterval time.Duration `default:"60s" split_words:"true"`
-	DeviceDiscoveryTimeout  time.Duration `default:"30s" split_words:"true"`
+	Address           string        `default:"localhost:6060" desc:"Address for the webhook listener in host:port form." required:"false"`
+	DiscoveryInterval time.Duration `default:"60s" split_words:"true" desc:"How often to start a new discovery job." required:"false"`
+	DiscoveryTimeout  time.Duration `default:"30s" split_words:"true" desc:"How long a discovery job should run for." required:"false"`
 }
 
 func main() {
+	var help bool
+	flag.BoolVarP(&help, "help", "h", false, "Show the help information and exit")
+	flag.Parse()
 	var c Config
 	err := envconfig.Process("PLEXIFX", &c)
 	if err != nil {
 		panic(fmt.Errorf("could not parse config: %s", err))
 	}
 
-	if c.DeviceDiscoveryInterval <= c.DeviceDiscoveryTimeout {
+	if help {
+		err = envconfig.Usagef("PLEXIFX", &c, os.Stdout, envconfig.DefaultListFormat)
+		if err != nil {
+		}
+		os.Exit(0)
+	}
+
+	if c.DiscoveryInterval <= c.DiscoveryTimeout {
 		panic("discovery interval cannot be less than or equal to discovery timeout")
 	}
 
@@ -35,7 +47,7 @@ func main() {
 	}
 	defer logger.Sync()
 
-	spew.Printf("Discovery interval %v: , Discovery timeout: %v\n", c.DeviceDiscoveryInterval, c.DeviceDiscoveryTimeout)
+	spew.Printf("Discovery interval %v: , Discovery timeout: %v\n", c.DiscoveryInterval, c.DiscoveryTimeout)
 	// Start our webhook listener
-	webhook.New(c.Address, lifx.New(logger, c.DeviceDiscoveryInterval, c.DeviceDiscoveryTimeout), logger).Listen()
+	webhook.New(c.Address, lifx.New(logger, c.DiscoveryTimeout, c.DiscoveryInterval), logger).Listen()
 }
